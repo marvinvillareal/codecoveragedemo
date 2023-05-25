13c2d26b62cacaab40d24be19fbbf3e091c9cd37
Marvin Villareal <babyboymarvin@gmail.com>
2023-05-25 19:48:40 +0800

    Initial commit project

---
+++ CodeCoverageDemo/CodeCoverageDemo/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CodeCoverageDemo
+//
+//  Created by baby.boy.m.villareal on 5/23/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ CodeCoverageDemo/CodeCoverageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeCoverageDemo
+//
+//  Created by baby.boy.m.villareal on 5/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func myFunc() {
+        var x = 1
+        var y = 2
+        debugPrint(x+y)
+    }
+
+
+}
+
